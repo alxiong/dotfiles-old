c65619d2b98bc790b5e13b3d79d0241445a601b2
AlexXiong97 <veralexxiong@outlook.com>
2019-02-10 23:13:47 +0800

    improvement and fix based on Kali bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,69 @@
 #!/usr/bin/env bash
+pwd=$(pwd)
 
 git pull origin master;
-# sync up prezto, vimrc submodules
-git submodule update --remote;
+git submodule update --init --recursive;
 
 # Set up zsh using Prezto Framework: https://github.com/AlexXiong97/prezto
 cd prezto/;
-rsync -avzh ./* ${ZDOTDIR:-$HOME}/.zprezto;
+rsync -avzh ./ ~/.zprezto;
+zsh;
+
+# install rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv;
+cd ~/.rbenv && src/configure && make -C src;
+## NOTE: rbenv path is already added into ~/.zshenv
+source ~/.zshrc;
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash; # check rbenv install status
+
+# install virtualenvwrapper
+pip install virtualenvwrapper;
+
 ## creating new zsh configuration:
-setopt EXTENDED_GLOB;
+echo "setopt EXTENDED_GLOB;
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+done" >> ~/.zshrc;
 ## set zsh as the default shell:
 chsh -s /bin/zsh;
-cd ../;
+
+# setup prezto-contrib
+cd $ZPREZTODIR
+git clone https://github.com/belak/prezto-contrib contrib
+cd contrib
+git submodule init
+git submodule update
+
+cd pwd;
 
 # Set up VIM using vimrc: https://github.com/AlexXiong97/vimrc
 cd vimrc/;
-rsync -avzh ./* ~/.vim_runtime;
+rsync -avzh ./ ~/.vim_runtime;
 sh ~/.vim_runtime/install_awesome_vimrc.sh;
 cd ../;
+
+# install Fira fonts
+fonts_dir="${HOME}/.local/share/fonts"
+if [ ! -d "${fonts_dir}" ]; then
+    echo "mkdir -p $fonts_dir"
+    mkdir -p "${fonts_dir}"
+else
+    echo "Found fonts dir $fonts_dir"
+fi
+
+for type in Bold Light Medium Regular Retina; do
+    file_path="${HOME}/.local/share/fonts/FiraCode-${type}.ttf"
+    file_url="https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true"
+    if [ ! -e "${file_path}" ]; then
+        echo "wget -O $file_path $file_url"
+        wget -O "${file_path}" "${file_url}"
+    else
+	echo "Found existing file $file_path"
+    fi;
+done
+
+echo "fc-cache -f"
+fc-cache -f
 
 # Copying other dotfiles
 function doIt() {
